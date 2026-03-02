7f090ac9ccaa1f5711136109d57a309851e4eeab
Suhail-Sharieff <suhailsharieffsharieff@gmail.com>
2026-03-02 20:49:05 +0530

    test

--- backend/test/run.js
+++ backend/test/run.js
@@ -1,14 +1,13 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
 //k6 run --vus 10 --duration 30s script.js
 export const options = {
-  iterations: 60,
+  vus:1,
+  iterations: 4,
 };
 
 // The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
 export default function () {
-  // Make a GET request to the target URL
-//   http.get('http://localhost:8080/auth/me');
   const payload = JSON.stringify({
     email: "suhailsharieffsharieff@gmail.com",
     password: "Test@123"
@@ -20,7 +19,14 @@ export default function () {
     }
   };
 
-  http.post('http://localhost:8080/auth/login', payload, params);
+  const res=http.post('http://localhost:8080/auth/login', payload, params);
+  check(res,{
+    is_status_200:(r)=>r.status===200,
+    "res has accessToken":(r)=>r.body.includes("accessToken")
+  })
+
+
   // Sleep for 1 second to simulate real-world usage
+
   sleep(0.1);
 }
